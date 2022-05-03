f9cc1b9cfcea5456e6f6fd32d62087ed986d06a2
aciernoantonio <aciernoantonio.int@gmail.com>
2022-05-03 15:43:39 +0200

    Initial commit with nav import error

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+
+  <SiteHeader/>
+
+    
+
+  </div>
+</template>
+
+<script>
+
+import SiteHeader from "@/components/headerComponent.vue"
+
+
+export default {
+  name: 'headerComponent',
+  components: {
+    SiteHeader
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+
+
+</style>
---
+++ src/components/headerComponent.vue
@@ -0,0 +1,84 @@
+<template>
+    
+    <header>
+
+    <nav>
+
+      <div class="container">
+
+      <div class="d-flex center navbar">
+
+        <div>
+          <img src="./assets/dc-logo.png" alt="">
+        </div>
+        <div class="navLink">
+
+          <a href="">CHARACTERS</a>
+          <a class="active" href="">COMICS</a>
+          <a href="">MOVIES</a>
+          <a href="">TV</a>
+          <a href="">GAMES</a>
+          <a href="">COLLECTIBLES</a>
+          <a href="">VIDEOS</a>
+          <a href="">FAMS</a>
+          <a href="">NEWS</a>
+          <a href="">SHOP</a>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    </nav>
+
+  </header>
+    
+</template>
+
+<script>
+
+export default {
+  name: "app",
+  components: {
+    
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+@import "@/assets/scss/style";
+
+    nav{
+
+  a{
+    margin: 0 0.75rem;
+    text-decoration: none;
+    color: black;
+  }
+
+  img{
+    width: 75%;
+    padding: 1rem 0;
+  }
+
+  .navLink {
+    font-size: 0.75rem;
+    font-weight: bold;
+  }
+
+  .navbar{
+    justify-content: space-around;
+  }
+
+  .active{
+    color: $primay-color;
+    border-bottom: 7px solid $primary-color;
+    padding-bottom: 2rem
+  }
+
+}
+
+</style>
